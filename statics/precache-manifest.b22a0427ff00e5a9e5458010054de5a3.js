11b44541b5eb3bdb1fae5ca5530f24c3d6a8d152
lei.zhang <lei.zhang@sincerecloud.com>
2023-09-15 14:32:07 +0800

    init

---
+++ statics/precache-manifest.b22a0427ff00e5a9e5458010054de5a3.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507b1bb56813572fe70f44dd561fcbb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059e2aec47491ef41d41",
+    "url": "/static/css/6.45a5650e.chunk.css"
+  },
+  {
+    "revision": "2067cb3fbfd8deb43207",
+    "url": "/static/js/10.3dcbdf43.chunk.js"
+  },
+  {
+    "revision": "936b303924b85a36e3d1",
+    "url": "/static/js/11.43ab61bd.chunk.js"
+  },
+  {
+    "revision": "fef897c6591827312470",
+    "url": "/static/js/12.0e44b920.chunk.js"
+  },
+  {
+    "revision": "63b2469f7eb9c0464488",
+    "url": "/static/js/13.af011761.chunk.js"
+  },
+  {
+    "revision": "059e2aec47491ef41d41",
+    "url": "/static/js/6.e9a0eb24.chunk.js"
+  },
+  {
+    "revision": "aa12a8aade1f14f95f59",
+    "url": "/static/js/7.f4477e6e.chunk.js"
+  },
+  {
+    "revision": "65034751f0c69123c459",
+    "url": "/static/js/8.6bd6d92d.chunk.js"
+  },
+  {
+    "revision": "d606961db76e04a74f72",
+    "url": "/static/js/9.0c3fce01.chunk.js"
+  },
+  {
+    "revision": "4cf8f96cc67889380419",
+    "url": "/static/js/codeEditor.5e784d55.chunk.js"
+  },
+  {
+    "revision": "7274b1d285db9525c6b8",
+    "url": "/static/js/main.8db160ee.chunk.js"
+  },
+  {
+    "revision": "7285788f777a73ea47ff",
+    "url": "/static/js/runtime-main.d5cf029b.js"
+  },
+  {
+    "revision": "51c934afb61ee321ae0d9f05a4b432e9",
+    "url": "/static/media/getFetch.51c934af.cjs"
+  }
+]);
